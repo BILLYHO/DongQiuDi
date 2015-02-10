933e6f7278c0aaa70d3b7a1737310ee834d710b1
BILLYHO <billyho0906@gmail.com>
2015-02-10 11:15:26 +0800

    weibo oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,6 +26,7 @@ class SessionsController < ApplicationController
     # send the request
     resp = Net::HTTP.post_form(url, post_args)
     puts resp.body
+    flash[:success] = resp.body.to_s
     #redirect_to "https://api.weibo.com/oauth2/access_token?client_id=3741023176&client_secret=1d9a681ae216b72f2baa31a03390777c&grant_type=authorization_code&redirect_uri=http://dongqiudi.herokuapp.com/oauth&code=#{p['code'].first}"
   end
 
