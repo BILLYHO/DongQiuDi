fd5626a53283770f877de309b602714afecfdecf
BILLYHO <billyho0906@gmail.com>
2015-02-14 22:04:47 +0800

    chang route to post

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
 
-  get 'wechat' => 'wechat#response'
+  post 'wechat' => 'wechat#response'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
