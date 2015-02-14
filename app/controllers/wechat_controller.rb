c863cdb1ded9d35a3289dfa6394d0992c474f243
BILLYHO <billyho0906@gmail.com>
2015-02-14 22:10:27 +0800

    wechat

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,11 +1,11 @@
 class WechatController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def init
+  def show
     render :text => params[:echostr]
   end
 
-  def response
+  def create
     if params[:xml][:MsgType] == "text"
       render "echo", :formats => :xml
     end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
 
-  post 'wechat' => 'wechat#response'
+  resources :wechat
+
+  #get 'wechat' => 'wechat#init'
+  #post 'wechat' => 'wechat#response'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
